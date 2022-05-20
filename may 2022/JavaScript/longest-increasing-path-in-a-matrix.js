c92a7d8f4af7c4330433f91859dcde9729309a1f
John Q. Herman <95893344+JohnQHerman@users.noreply.github.com>
2022-05-20 11:21:40 -0400

    Update longest-increasing-path-in-a-matrix.js
    reverted previous changes, an iterative function works better

--- may 2022/JavaScript/longest-increasing-path-in-a-matrix.js
+++ may 2022/JavaScript/longest-increasing-path-in-a-matrix.js
@@ -29,23 +29,20 @@
 
 // let's use a depth-first search to find the longest increasing path in the matrix
 
-// set up a dfs helper function that will return the max of all the dfs calls
+// set up a dfs helper function and return the max
 dfs = function(i, j, matrix, dp){
     if(dp[i][j] !== 0) return dp[i][j];
     let max = 1;
-    switch(true){
-        case i > 0 && matrix[i][j] > matrix[i-1][j]: max = Math.max(max, 1 + dfs(i-1, j, matrix, dp));
-        case i < matrix.length-1 && matrix[i][j] > matrix[i+1][j]: max = Math.max(max, 1 + dfs(i+1, j, matrix, dp));
-        case j > 0 && matrix[i][j] > matrix[i][j-1]: max = Math.max(max, 1 + dfs(i, j-1, matrix, dp));
-        case j < matrix[0].length-1 && matrix[i][j] > matrix[i][j+1]: max = Math.max(max, 1 + dfs(i, j+1, matrix, dp));
-    }
+    if(i > 0 && matrix[i][j] > matrix[i-1][j]) max = Math.max(max, 1 + dfs(i-1, j, matrix, dp));
+    if(i < matrix.length-1 && matrix[i][j] > matrix[i+1][j]) max = Math.max(max, 1 + dfs(i+1, j, matrix, dp));
+    if(j > 0 && matrix[i][j] > matrix[i][j-1]) max = Math.max(max, 1 + dfs(i, j-1, matrix, dp));
+    if(j < matrix[0].length-1 && matrix[i][j] > matrix[i][j+1]) max = Math.max(max, 1 + dfs(i, j+1, matrix, dp));
     dp[i][j] = max;
     return max;
 }
 
-// find the max of all the dfs calls and return it as the answer
+// find the max of all the dfs calls and return it
  var longestIncreasingPath = function(matrix) {
-     // set up a dp array to store the max of each dfs call
     if(matrix.length === 0) return 0;
     let dp = [];
     for(let i = 0; i < matrix.length; i++){
@@ -55,10 +52,10 @@ dfs = function(i, j, matrix, dp){
         }
     }
     let max = 0;
-    for(let i = 0; i < matrix.length; i++){ // loop through the matrix and call the dfs helper function on each cell
-        for(let j = 0; j < matrix[0].length; j++){ // dfs helper function will return the max of all the dfs calls
-            max = Math.max(max, dfs(i, j, matrix, dp)); // call the dfs helper function
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[0].length; j++){
+            max = Math.max(max, dfs(i, j, matrix, dp));
         }
     }
     return max;
-};
+}; 
